e314569f55baaf71c9119dd309a6768546f89339
sealday <sealday@gmail.com>
2015-05-25 01:40:08 +0800

    add dummy things

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,13 @@
 
 #include <GLFW/glfw3.h>
 
+#include "camera.h"
+
 
 void error_callback(int error, const char *description);
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
 
 int main() {
-
 
     glfwSetErrorCallback(error_callback);
     if (!glfwInit()) {
