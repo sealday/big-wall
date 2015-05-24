c96bf34cbaee08a38a4398d6540e19869d60a123
sealday <sealday@gmail.com>
2015-05-24 20:55:48 +0800

    add glew support

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-#define GLEW_STATIC
 #include <GL/glew.h>
 
 #include <GLFW/glfw3.h>
@@ -16,6 +15,13 @@ int main() {
     if (!glfwInit()) {
         std::cout << "can't not init glfw" << std::endl;
     }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // for mac
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
 
     GLFWwindow *window = glfwCreateWindow(800, 600, "opengl", NULL, NULL);
     if (!window)
@@ -27,11 +33,17 @@ int main() {
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+
     glfwSetKeyCallback(window, key_callback);
 
     while (!glfwWindowShouldClose(window)) {
-
-
 
         glfwSwapBuffers(window);
         glfwPollEvents();
