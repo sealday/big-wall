72be747dee1d988e3d4ac6e8f415c282a16a83b9
sealday <sealday@gmail.com>
2015-05-25 01:39:47 +0800

    add glm

---
+++ include/camera.h
@@ -0,0 +1,8 @@
+//
+// Created by seal on 15-5-24.
+//
+
+#ifndef BIG_WALL_CAMERA_H
+#define BIG_WALL_CAMERA_H
+
+#endif //BIG_WALL_CAMERA_H
---
+++ include/shader_strings.h
@@ -0,0 +1,8 @@
+//
+// Created by seal on 15-5-25.
+//
+
+#ifndef BIG_WALL_SHADER_STRINGS_H
+#define BIG_WALL_SHADER_STRINGS_H
+
+#endif //BIG_WALL_SHADER_STRINGS_H
